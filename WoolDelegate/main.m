d46c952a10d6d85430c9147aedd40fcbfc007c7d
Joshua Caswell <josh@kraken.(none)>
2011-12-06 15:40:46 -0800

    Initial Commit

---
+++ WoolDelegate/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WoolDelegate
+//
+//  Created by Joshua Caswell on 12/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
